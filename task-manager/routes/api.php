953e894faff77703633c7e6fbe093129bc1a8dc5
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-27 18:40:59 -0300

    feat: criado rota para editar por id

--- task-manager/routes/api.php
+++ task-manager/routes/api.php
@@ -13,6 +13,7 @@ Route::group(['middleware' => 'api'], function ($router){
     Route::get('task', [TaskController::class, 'getList']);
     Route::post('task', [TaskController::class, 'store']);
     Route::put('users/update/{id}', [TaskController::class, 'update']);
+    Route::delete('users/delete/{id}', [TaskController::class, 'destroy']);
     
 
 });
