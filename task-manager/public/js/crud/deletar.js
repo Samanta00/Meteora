79c3cb255b0dfe087e7a04d0681cf1d8aeaa7747
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-28 17:30:15 -0300

    feat: criado função para deletar tarefa

---
+++ task-manager/public/js/crud/deletar.js
@@ -0,0 +1,17 @@
+        // Função para deletar uma tarefa
+        async function deletarTarefa(taskId) {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/task/delete/${taskId}`, {
+                    method: 'DELETE'
+                });
+
+                if (response.ok) {
+                    console.log(`Tarefa com ID ${taskId} deletada com sucesso.`);
+                    fetchTasks(); // Atualiza a lista após a remoção da tarefa
+                } else {
+                    console.error(`Erro ao deletar a tarefa com ID ${taskId}.`);
+                }
+            } catch (error) {
+                console.error('Erro ao enviar a requisição:', error);
+            }
+        }
