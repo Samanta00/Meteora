9dd6e9db36a500ed4f1109f7190cb755d3969ed6
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-28 16:58:45 -0300

    lint: organização de função e html para criar tarefas

--- task-manager/resources/views/anotacao.blade.php
+++ task-manager/resources/views/anotacao.blade.php
@@ -53,7 +53,8 @@
         </div>
     </div>
 
-    <script src="../js/crud/adicionar.js"></script>
+    @yield('constent')
+    <script src="/js/crud/adicionar.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('addTaskBtn').addEventListener('click', adicionarTarefas);
@@ -61,39 +62,7 @@
             fetchTasks();
         });
 
-        // Função para adicionar tarefas
-        async function adicionarTarefas(event) {
-            event.preventDefault();
-        
-            const title = document.getElementById('task-title').value;
-            const description = document.getElementById('task-description').value;
-            const completed = document.getElementById('task-completed').checked;
-
-            const data = {
-                title: title,
-                description: description,
-                completed: completed
-            };
-
-            try {
-                const response = await fetch('http://127.0.0.1:8000/api/task', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
-
-                if (response.ok) {
-                    console.log('Tarefa criada com sucesso!');
-                    // Aqui você pode adicionar lógica para atualizar a interface
-                } else {
-                    console.error('Erro ao criar a tarefa.');
-                }
-            } catch (error) {
-                console.error('Erro ao enviar a requisição:', error);
-            }
-        }
+   
 
         
 
