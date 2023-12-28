d209d33dfa0d2dfc6920fc283dd3028c459ff0d5
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-28 18:42:29 -0300

    lint: limpeza de chamado repetitivo do yield

--- task-manager/resources/views/anotacao.blade.php
+++ task-manager/resources/views/anotacao.blade.php
@@ -38,7 +38,7 @@
 
 
         <!-- Lista de tarefas existentes -->
-        <div class="task-list" id="taskList">
+        <div >
             <h3>Tarefas Salvas</h3>
             <form class="task-id-form">
                 <label for="task-id">Filtrar por ID:</label>
@@ -67,128 +67,17 @@
 
     @yield('constent')
     <script src="/js/crud/adicionar.js"></script>
-    @yield('constent')
     <script src="/js/crud/visualizarList.js"></script>
-    @yield('constent')
     <script src="/js/crud/deletar.js"></script>
-    
+    <script src="/js/crud/edicao.js"></script>
+    <script src="/js/crud/filtro.js"></script>
+
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('addTaskBtn').addEventListener('click', adicionarTarefas);
             document.getElementById('filterByIdBtn').addEventListener('click', filtrarPorId);
             fetchTasks();
         });
-
-
-
-
-        // Função para filtrar por ID
-        async function filtrarPorId() {
-            const id = document.getElementById('filter-id').value;
-            if (id.trim() !== '') {
-                try {
-                    const response = await fetch(`http://127.0.0.1:8000/api/task/${id}`);
-                    if (response.ok) {
-                        const task = await response.json();
-                        displayTasks([task]);
-                    } else {
-                        console.error('Erro ao buscar a tarefa por ID:', response.statusText);
-                    }
-                } catch (error) {
-                    console.error('Erro ao buscar a tarefa por ID:', error);
-                }
-            } else {
-                fetchTasks(); // Se o campo estiver vazio, mostra todas as tarefas
-            }
-        }
-
-        // Função para editar a tarefa pelo ID
-        function editarTarefa(id) {
-
-            console.log(`Editar tarefa com ID ${id}`);
-        }
-
- 
-
-        // Função para buscar os detalhes de uma tarefa por ID
-        async function buscarDetalhesTarefa(taskId) {
-            try {
-                const response = await fetch(`http://127.0.0.1:8000/api/task/view/${taskId}`);
-                if (response.ok) {
-                    const taskDetails = await response.json();
-                    exibirFormularioEdicao(taskDetails);
-                } else {
-                    console.error(`Erro ao buscar os detalhes da tarefa com ID ${taskId}.`);
-                }
-            } catch (error) {
-                console.error('Erro ao buscar os detalhes da tarefa:', error);
-            }
-        }
-
-        // Função para exibir um formulário com os detalhes da tarefa para edição
-        function exibirFormularioEdicao(taskDetails) {
-            const formEdicao = document.createElement('form');
-            formEdicao.classList.add('edit-form');
-
-            const titleInput = document.createElement('input');
-            titleInput.setAttribute('type', 'text');
-            titleInput.setAttribute('value', taskDetails.title);
-
-            const descriptionTextarea = document.createElement('textarea');
-            descriptionTextarea.textContent = taskDetails.description;
-
-            const completedCheckbox = document.createElement('input');
-            completedCheckbox.setAttribute('type', 'checkbox');
-            completedCheckbox.checked = taskDetails.completed;
-
-            const submitBtn = document.createElement('button');
-            submitBtn.textContent = 'Salvar';
-
-            formEdicao.append(titleInput, descriptionTextarea, completedCheckbox, submitBtn);
-
-            // Adicionar lógica para submeter os dados atualizados para a API
-            formEdicao.addEventListener('submit', async (event) => {
-                event.preventDefault();
-
-                // Obter os novos valores do formulário de edição
-                const editedData = {
-                    title: titleInput.value,
-                    description: descriptionTextarea.value,
-                    completed: completedCheckbox.checked
-                };
-
-                try {
-                    const response = await fetch(`http://127.0.0.1:8000/api/task/update/${taskDetails.id}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(editedData)
-                    });
-
-                    if (response.ok) {
-                        console.log(`Tarefa com ID ${taskDetails.id} atualizada com sucesso.`);
-                        fetchTasks(); // Atualiza a lista após a edição da tarefa
-                    } else {
-                        console.error(`Erro ao atualizar a tarefa com ID ${taskDetails.id}.`);
-                    }
-                } catch (error) {
-                    console.error('Erro ao enviar a requisição:', error);
-                }
-            });
-
-            const taskListContainer = document.getElementById('taskList');
-            taskListContainer.innerHTML = '';
-            taskListContainer.appendChild(formEdicao);
-        }
-
-        // ... (restante do código)
-
-        // Função para editar a tarefa pelo ID
-        function editarTarefa(id) {
-            buscarDetalhesTarefa(id);
-        }
     </script>
 </body>
-
 </html>
