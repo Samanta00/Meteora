98de48ffec856e91c458f209c32dccb68398be8e
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-29 09:33:36 -0300

    lint: remoção da função para encontrar por id

--- task-manager/public/js/crud/filtro.js
+++ task-manager/public/js/crud/filtro.js
@@ -1,21 +1,4 @@
-async function filtrarPorId() {
-    const id = document.getElementById('filter-id').value;
-    if (id.trim() !== '') {
-        try {
-            const response = await fetch(`http://127.0.0.1:8000/api/task/${id}`);
-            if (response.ok) {
-                const task = await response.json();
-                displayTasks([task]);
-            } else {
-                console.error('Erro ao buscar a tarefa por ID:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Erro ao buscar a tarefa por ID:', error);
-        }
-    } else {
-        fetchTasks(); // Se o campo estiver vazio, mostra todas as tarefas
-    }
-}
+
 
 
 let filteredTasks = []; // Variável para armazenar os resultados filtrados
@@ -88,7 +71,6 @@ function displayFilteredTasks() {
         const itemActions = document.createElement('div');
         itemActions.classList.add('item-actions');
 
-        // ... Criação dos botões editar e remover
 
         const taskElement = document.createElement('div');
         taskElement.classList.add('task');
