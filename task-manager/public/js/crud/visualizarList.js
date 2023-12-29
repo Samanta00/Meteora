501b803217533e8c47c8fa76c6708725837a30c1
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-29 10:47:19 -0300

    lint: organizando e limpando interface para ficar mais apresentável a lista de tarefas

--- task-manager/public/js/crud/visualizarList.js
+++ task-manager/public/js/crud/visualizarList.js
@@ -17,47 +17,53 @@
         function displayTasks(tasks) {
             const taskList = document.getElementById('taskList');
             taskList.innerHTML = ''; // Limpa a lista antes de adicionar as tarefas
-
+        
             tasks.forEach(task => {
                 const cartItem = document.createElement('div');
                 cartItem.classList.add('cart-item');
-
+        
                 const itemActions = document.createElement('div');
                 itemActions.classList.add('item-actions');
-
+        
                 const editBtn = document.createElement('button');
                 editBtn.classList.add('edit-button');
                 editBtn.textContent = 'Editar';
                 editBtn.addEventListener('click', () => editarTarefa(task.id)); // Chama a função para editar a tarefa
-
+        
                 const removeBtn = document.createElement('button');
                 removeBtn.classList.add('remove-button');
                 removeBtn.textContent = 'Remover';
                 removeBtn.addEventListener('click', () => deletarTarefa(task.id)); // Chama a função para remover a tarefa
-
+        
                 itemActions.appendChild(editBtn);
                 itemActions.appendChild(removeBtn);
-
+        
                 const taskElement = document.createElement('div');
                 taskElement.classList.add('task');
-
+        
                 const title = document.createElement('h3');
-                title.textContent = task.title;
-
+                title.textContent = `Título: ${task.title}`;
+        
                 const description = document.createElement('p');
-                description.textContent = task.description;
-
-                const status = document.createElement('span');
-                status.classList.add('task-status');
-                status.textContent = task.completed ? 'Concluída' : 'Pendente';
-
+                description.textContent = `Descrição: ${task.description}`;
+        
+                const status = document.createElement('div');
+                status.classList.add('status');
+                status.innerHTML = `Status:${task.status}`; // Altere para a propriedade correta que contém o status da tarefa
+        
+                const completion = document.createElement('div');
+                completion.classList.add('completion');
+                completion.innerHTML = `<span>${task.completed ? 'Concluída' : 'Pendente'}</span>`;
+        
                 taskElement.appendChild(title);
                 taskElement.appendChild(description);
                 taskElement.appendChild(status);
-
+                taskElement.appendChild(completion);
+        
                 cartItem.appendChild(itemActions);
                 cartItem.appendChild(taskElement);
-
+        
                 taskList.appendChild(cartItem);
             });
         }
+        
