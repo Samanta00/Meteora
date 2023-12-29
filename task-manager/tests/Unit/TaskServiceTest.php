8f8dc977888b3f128e61b8488696c5f908851f4c
Ellen Samanta <ellen.samanta@outlook.com>
2023-12-29 13:28:15 -0300

    lint: comentando código de testes

--- task-manager/tests/Unit/TaskServiceTest.php
+++ task-manager/tests/Unit/TaskServiceTest.php
@@ -14,12 +14,12 @@ class TaskServiceTest extends TestCase
     {
         // Criando um mock para a classe TaskService
         $taskServiceMock = $this->getMockBuilder(TaskService::class)
-                                ->disableOriginalConstructor()
-                                ->getMock();
+            ->disableOriginalConstructor()
+            ->getMock();
 
         // Configurando o mock para retornar uma instância de Task
         $taskServiceMock->method('store')
-                        ->willReturn(new Task(['title' => 'Nova Tarefa', 'description' => 'Descrição da nova tarefa']));
+            ->willReturn(new Task(['title' => 'Nova Tarefa', 'description' => 'Descrição da nova tarefa']));
 
         // Chama o método store do TaskService (simulado)
         $storedTask = $taskServiceMock->store(['title' => 'Nova Tarefa', 'description' => 'Descrição da nova tarefa']);
@@ -37,7 +37,7 @@ class TaskServiceTest extends TestCase
         $taskMock->shouldReceive('all')->andReturn([
             new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']),
             new Task(['title' => 'Tarefa 2', 'description' => 'Descrição da Tarefa 2']),
-          
+
         ]);
 
         $taskService = new TaskService($taskMock);
@@ -51,60 +51,67 @@ class TaskServiceTest extends TestCase
 
     public function test_get_task_by_id(): void
     {
-      
+        // Cria um mock para a classe Task
         $taskMock = Mockery::mock(Task::class);
+
+        // Configura o mock para simular o método findOrFail retornando uma instância de Task
         $taskMock->shouldReceive('findOrFail')
-                 ->with(1) 
-                 ->andReturn(new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']));
+            ->with(1)
+            ->andReturn(new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']));
 
-
+        // Cria uma instância de TaskService com o mock criado
         $taskService = new TaskService($taskMock);
 
-        $task = $taskService->get(1); 
+        // Chama o método get do TaskService com o ID 1
+        $task = $taskService->get(1);
 
+        // Verifica se o retorno é uma instância de Task
         $this->assertInstanceOf(Task::class, $task);
     }
 
     public function test_update_task(): void
     {
-        
+        // Cria um mock para a classe Task
         $taskMock = Mockery::mock(Task::class);
 
+        // Configura o mock para simular o método findOrFail retornando uma instância de Task
         $taskMock->shouldReceive('findOrFail')
-                 ->with(1) 
-                 ->andReturn(new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']));
+            ->with(1)
+            ->andReturn(new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']));
 
-     
+        // Configura o mock para simular o método update
         $taskMock->shouldReceive('where->update')
-                 ->with(['title' => 'Título Atualizado']) 
-                 ->once(); 
+            ->with(['title' => 'Título Atualizado'])
+            ->once();
 
-      
+        // Cria uma instância de TaskService com o mock criado
         $taskService = new TaskService($taskMock);
 
-        $updatedTask = $taskService->update(['title' => 'Título Atualizado'], 1); 
+        // Chama o método update do TaskService com um novo título e o ID 1
+        $updatedTask = $taskService->update(['title' => 'Título Atualizado'], 1);
+
+        // Verifica se o retorno é uma instância de Task
         $this->assertInstanceOf(Task::class, $updatedTask);
     }
 
+
     public function test_destroy_task_exists(): void
     {
+        // Cria um mock para a classe Task
         $taskMock = Mockery::mock(Task::class);
+
+        // Configura o mock para simular o método find retornando uma instância de Task
         $taskMock->shouldReceive('find')
-                 ->with(1) 
-                 ->andReturn(new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']));
-    
+            ->with(1)
+            ->andReturn(new Task(['title' => 'Tarefa 1', 'description' => 'Descrição da Tarefa 1']));
+
+        // Cria uma instância de TaskService com o mock criado
         $taskService = new TaskService($taskMock);
-        $result = $taskService->destroy(1); 
+
+        // Chama o método destroy do TaskService com o ID 1
+        $result = $taskService->destroy(1);
+
+        // Verifica se o resultado é "Deletado com sucesso"
         $this->assertEquals("Deletado com sucesso", $result);
     }
-
-
-    
-
-
-    
- 
-    
 }
-
-
